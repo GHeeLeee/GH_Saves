b9508e9d2aaef9c7f3eef6ff5dd795ed7f5ec817
GHeeLeee <134215576+GHeeLeee@users.noreply.github.com>
2023-06-06 23:35:33 +0900

    06.06 Commit
    1. 예매한 영화 표시 UI 제작
     - 홈 화면 UI
     - 내 정보 UI
    
    2. 디자인 추가
     - btn_line2
     - btn_line3
    
    3. 좌석 선택 UI 수정
     - 디자인 변경
    
    4. 극장 지역 선택 UI 제작
    
    5. 극장 선택 UI 제작

---
+++ app/src/main/java/com/example/movie/TheaterChoiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.movie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TheaterChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theater_choice);
+    }
+}
